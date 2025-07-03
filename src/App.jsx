1f59a176be8592125fae313e9112a45cbb78acee
Zilong Wang <zilong.wang@ibm.com>
2025-07-03 14:53:03 -0400

    add a comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import MainContent from './components/MainContent';
-import logo from './assets/images/logo.png';
+import MainContent from "./components/MainContent";
+import logo from "./assets/images/logo.png";
 
 function App() {
   return (
@@ -14,5 +14,7 @@ function App() {
     </>
   );
 }
+
+// this is some comment
 
 export default App;
